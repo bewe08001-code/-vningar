5809f741261975a4a5abf67727cff3a21a52dcb0
bewe08001-code <bewe08001@utb.vaxjo.se>
2025-08-29 14:16:37 +0200

    Övningarna 1-3.cs

--- Övning1-1.cs
+++ Övning1-1.cs
@@ -11,9 +11,41 @@ namespace Lektion1
             string svar = Console.ReadLine();
             if (svar == "y")
             {
-                    Console.Clear();
-                    Console.WriteLine("\" \\ / \n \t");
-            
+                Console.Clear();
+                Console.WriteLine("\" \\ / \n \t");
+                Console.WriteLine("Vill du ha ett virus (det är ofarligt) (y/n)");
+                string svar1 = Console.ReadLine();
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                if (svar1 == "y")
+                {
+                    Console.WriteLine("Grattis du har fått ett virus!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Du får ett ändå");
+                    Console.ReadLine();
+                }
+                Console.Clear();
+                Console.WriteLine("Nu är din dator blå med vita bokstäver");
+                Console.WriteLine("Det betyder att du har fått ett virus");
+
+                Console.WriteLine("Vill du även se en fin triangel? (y/n)");
+                string svar2 = Console.ReadLine();
+                Console.ResetColor();
+                Console.Clear();
+                if (svar2 == "y")
+                {
+                    Console.WriteLine("   /\\   ");
+                    Console.WriteLine("  /  \\  ");
+                    Console.WriteLine(" /\" \" \\ ");
+                    Console.WriteLine("/______\\");
+                }
+                else
+                {
+                }    
             }
             else
             {
